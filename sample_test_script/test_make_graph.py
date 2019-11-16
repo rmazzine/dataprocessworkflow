37ce86890ca087024659ad9550c7993dabbb1b35
rmazzine <43969088+rmazzine@users.noreply.github.com>
2019-11-16 15:03:00 -0300

    Adding sys path append on sample test

--- sample_test_script/test_make_graph.py
+++ sample_test_script/test_make_graph.py
@@ -1,3 +1,6 @@
+import sys
+sys.path.append("..")
+
 from dpworkflow import graph_generator
 
 graph_generator.graph('sample_test_df.py', graphviz2_path='C:/Program Files (x86)/Graphviz2.38/bin').create_graph()
